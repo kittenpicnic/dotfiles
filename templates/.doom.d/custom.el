4e90e8110be8d65236654613a87d2e28ca51bd57
pyxylyze <6722266+pyxylyze@users.noreply.github.com>
2021-04-06 12:36:41 +1000

    :sparkles: (emacs) add doom emacs config templates
    - add doom emacs custom config files

---
+++ templates/.doom.d/custom.el
@@ -0,0 +1,46 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#27212E" "#964C7B" "#74DFC4" "#FFE261" "#40B4C4" "#EB64B9" "#B4DCE7" "#FFFFFF"])
+ '(custom-safe-themes
+   '("c086fe46209696a2d01752c0216ed72fd6faeabaaaa40db9fc1518abebaf700d" "0a41da554c41c9169bdaba5745468608706c9046231bbbc0d155af1a12f32271" "01cf34eca93938925143f402c2e6141f03abb341f27d1c2dba3d50af9357ce70" default))
+ '(fci-rule-color "#544863")
+ '(jdee-db-active-breakpoint-face-colors (cons "#222228" "#40B4C4"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#222228" "#74DFC4"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#222228" "#4E415C"))
+ '(objed-cursor-color "#964C7B")
+ '(package-selected-packages '(sly-quicklisp))
+ '(pdf-view-midnight-colors (cons "#FFFFFF" "#27212E"))
+ '(rustic-ansi-faces
+   ["#27212E" "#964C7B" "#74DFC4" "#FFE261" "#40B4C4" "#EB64B9" "#B4DCE7" "#FFFFFF"])
+ '(vc-annotate-background "#27212E")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#74DFC4")
+    (cons 40 "#a2e0a3")
+    (cons 60 "#d0e182")
+    (cons 80 "#FFE261")
+    (cons 100 "#ffd35f")
+    (cons 120 "#ffc55d")
+    (cons 140 "#FFB85B")
+    (cons 160 "#f89c7a")
+    (cons 180 "#f18099")
+    (cons 200 "#EB64B9")
+    (cons 220 "#ce5ca4")
+    (cons 240 "#b2548f")
+    (cons 260 "#964C7B")
+    (cons 280 "#834973")
+    (cons 300 "#72466b")
+    (cons 320 "#604363")
+    (cons 340 "#544863")
+    (cons 360 "#544863")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
